6e07c1b195dea637fe4ff58c4015e2747341b3d6
lulinglm <lulinglm@163.com>
2019-08-15 14:52:02 +0800

    update icon

--- src/chrome/popup/index.js
+++ src/chrome/popup/index.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import AppComponent from './App/App.vue'
+import Vue from "vue";
+import AppComponent from "./App/App.vue";
 
-Vue.component('app-component', AppComponent)
+Vue.component("app-component", AppComponent);
 
-import { Card, Button } from 'element-ui'
+import { Card, Button } from "element-ui";
 
-Vue.use(Card)
-Vue.use(Button)
+Vue.use(Card);
+Vue.use(Button);
 
 new Vue({
-    el: '#app',
-    render: createElement => {
-        return createElement(AppComponent)
-    }
-})
+  el: "#app",
+  render: createElement => {
+    return createElement(AppComponent);
+  }
+});
